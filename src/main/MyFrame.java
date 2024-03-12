7b2020fac47d7adae4126bc5f5140d9e4a91eec0
jmarualgit <jairelmarual@gmail.com>
2024-03-12 10:33:00 -0700

    Set window starting location to center

--- src/main/MyFrame.java
+++ src/main/MyFrame.java
@@ -8,13 +8,14 @@ public class MyFrame extends JFrame {
     
     MyFrame () {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null);   // set screen starting position to center
 
         GamePanel gamePanel = new GamePanel();
         this.add(gamePanel);
         this.setBackground(Color.black);
         this.pack();
 
+        this.setLocationRelativeTo(null);   // set screen starting position to center
+        this.setResizable(false);
         this.setVisible(true);
 
         gamePanel.startGame();
